7945264a5b8b0a5d7e37701d406dcefcc672a9a2
irfanpule <irfan.pule2@gmail.com>
2019-11-18 08:56:17 +0700

    user courses enroll

--- nolsatu_courses/website/courses/views.py
+++ nolsatu_courses/website/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.decorators import login_required
 
 from nolsatu_courses.apps.courses.models import Courses
 
@@ -11,3 +12,13 @@ def details(request, slug):
         'course': course
     }
     return render(request, 'website/courses/details.html', context)
+
+
+@login_required
+def user_courses(request):
+    enrolls = request.user.enroll.all()
+    context = {
+        'title': _('Daftar Kursusmu'),
+        'courses': [enroll.course for enroll in enrolls]
+    }
+    return render(request, 'website/index.html', context)
