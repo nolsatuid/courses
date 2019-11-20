eab7e0e8235f3fc7ee80d06dcb55d31c1d303cab
irfanpule <irfan.pule2@gmail.com>
2019-11-20 14:12:11 +0700

    condition and enroll courses

--- nolsatu_courses/website/courses/views.py
+++ nolsatu_courses/website/courses/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from nolsatu_courses.apps.courses.models import Courses
 
@@ -9,7 +10,9 @@ def details(request, slug):
     course = get_object_or_404(Courses, slug=slug)
     context = {
         'title': course.title,
-        'course': course
+        'course': course,
+        'batch': course.get_last_batch(),
+        'has_enrolled': course.has_enrolled(request.user)
     }
     return render(request, 'website/courses/details.html', context)
 
@@ -22,3 +25,17 @@ def user_courses(request):
         'courses': [enroll.course for enroll in enrolls]
     }
     return render(request, 'website/index.html', context)
+
+
+@login_required
+def enroll(request, slug):
+    course = get_object_or_404(Courses, slug=slug)
+    if course.is_started():
+        messages.warning(
+            request, _(f'Gagal mendaftar, kelas {course.title} sudah dimulai')
+        )
+        return redirect('website:courses:details', course.slug)
+
+    course.enrolled.create(course=course, user=request.user)
+    messages.success(request, _(f'Kamu berhasil mendaftar pada kelas {course.title}'))
+    return redirect('website:courses:details', course.slug)
