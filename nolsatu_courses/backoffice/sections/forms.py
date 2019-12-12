153b593dfbf058715201f621b1966ac3ced4e431
Alin Nur Alifah <alinnurkhalifah@gmail.com>
2019-12-12 21:21:10 +0700

    fix backoffice

--- nolsatu_courses/backoffice/sections/forms.py
+++ nolsatu_courses/backoffice/sections/forms.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django import forms
 from nolsatu_courses.apps.courses.models import Section, TaskUploadSettings
 
@@ -28,3 +30,11 @@ class FormTaskSetting(forms.ModelForm):
     class Meta:
         model = TaskUploadSettings
         fields = ("instruction","allowed_extension","max_size")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data:
+            for ext in cleaned_data['allowed_extension']:
+                if not ext.startswith('.'):
+                    self.add_error('allowed_extension', _(f"Ekstensi {ext} harus menggunakan titik didepan"))
+        return cleaned_data
