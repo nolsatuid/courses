69f740f94ad207fce49edbd0842540798cc74fec
irfanpule <irfan.pule2@gmail.com>
2019-12-18 17:06:29 +0700

    remove unused function

--- nolsatu_courses/website/courses/views.py
+++ nolsatu_courses/website/courses/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils import timezone
 
-from nolsatu_courses.apps.decorators import enroll_required
 from nolsatu_courses.apps.courses.models import Courses, Enrollment
 
 
