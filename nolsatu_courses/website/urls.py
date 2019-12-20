63a20b716736c00c56a638b87925fcf989faa014
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2019-12-20 14:42:56 +0700

    add search page

--- nolsatu_courses/website/urls.py
+++ nolsatu_courses/website/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'website'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search', views.search, name='search'),
     path('tes_login/', views.test_login, name='test_login'),
     path('logout', views.logout, name='logout'),
     path('login', views.login, name='login'),
