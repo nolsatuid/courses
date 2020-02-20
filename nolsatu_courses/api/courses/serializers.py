7934d70629511892494fe07b90163c8c2244a5cd
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-02-20 18:15:03 +0700

    Course datail and set course api to public

--- nolsatu_courses/api/courses/serializers.py
+++ nolsatu_courses/api/courses/serializers.py
@@ -6,4 +6,10 @@ from nolsatu_courses.apps.courses.models import Courses
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
-        exclude = ['users', 'slug', 'is_visible']
+        exclude = ['users', 'slug', 'description', 'is_visible']
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Courses
+        exclude = ['users', 'slug', 'short_description', 'is_visible']
--- nolsatu_courses/api/courses/urls.py
+++ nolsatu_courses/api/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "courses"
 urlpatterns = [
     path('list', views.CourseListView.as_view(), name='list'),
+    path('detail/<int:id>', views.CourseDetailView.as_view(), name='detail'),
 ]
