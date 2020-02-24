be4fa354b734e0df96ce5f5f3182d4b54c384eaa
irfanpule <irfan.pule2@gmail.com>
2020-02-24 14:18:41 +0700

    api enroll course

--- nolsatu_courses/api/courses/urls.py
+++ nolsatu_courses/api/courses/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('list', views.CourseListView.as_view(), name='list'),
     path('detail/<int:id>', views.CourseDetailView.as_view(), name='detail'),
     path('detail/enroll/<int:id>', views.CourseEnrollDetailView.as_view(), name='detail_enroll'),
+    path('enroll/<slug:slug>', views.EnrollCourseView.as_view(), name='enroll'),
 ]
--- nolsatu_courses/api/courses/views.py
+++ nolsatu_courses/api/courses/views.py
@@ -3,12 +3,15 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from django.utils.translation import ugettext_lazy as _
+
 from nolsatu_courses.api.courses.serializers import (
-    CourseSerializer, CourseDetailSerializer, CourseDetailMergeSerializer,
-    CourseEnrollSerializer
+    CourseSerializer, CourseDetailMergeSerializer, CourseEnrollSerializer
 )
 from nolsatu_courses.api.authentications import UserAuthAPIView
+from nolsatu_courses.api.response import ErrorResponse
 from nolsatu_courses.apps.courses.models import Courses
+from nolsatu_courses.apps import utils
 
 
 class CourseListView(APIView):
@@ -41,7 +44,7 @@ class CourseDetailView(APIView):
 
 class CourseEnrollDetailView(UserAuthAPIView):
 
-    @swagger_auto_schema(tags=['Courses'], operation_description="To Enroll Course", responses={
+    @swagger_auto_schema(tags=['Courses'], operation_description="Enroll Detail", responses={
         200: CourseEnrollSerializer()
     })
     def get(self, request, id):
@@ -52,3 +55,30 @@ class CourseEnrollDetailView(UserAuthAPIView):
         }
         serializer = CourseEnrollSerializer(data)
         return Response(serializer.data)
+
+
+class EnrollCourseView(UserAuthAPIView):
+
+    @swagger_auto_schema(
+        tags=['Courses'], operation_description="To Enroll Course",
+        responses={200: "{message: 'Kamu berhasil terdaftar pada kelas .....'}"}
+    )
+    def get(self, request, slug):
+        course = get_object_or_404(Courses, slug=slug)
+
+        if course.has_enrolled(request.user):
+            return ErrorResponse(error_message=_(f'Kamu sudah terdaftar di kelas {course.title}'))
+
+        if not course.get_last_batch():
+            return ErrorResponse(error_message=_(f'Kelas {course.title} belum membuka pendaftaran'))
+
+        if course.is_started():
+            return ErrorResponse(error_message=_(f'Gagal mendaftar, kelas {course.title} sudah dimulai'))
+
+        course.enrolled.create(course=course, user=request.user, batch=course.batchs.last())
+        utils.send_notification(
+            request.user,
+            f'Kamu berhasil mendaftar di kelas {course.title}',
+            f'Saat ini kamu sudah berhasil mendaftar pada kelas {course.title}. Tunggu info selanjutnya ya.'
+        )
+        return Response({'message': _(f'Kamu berhasil terdaftar pada kelas {course.title}')})
