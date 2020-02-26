ee458434deca99586f372e822d412cf18896e8a1
irfanpule <irfan.pule2@gmail.com>
2020-02-26 07:21:42 +0700

    change URL

--- nolsatu_courses/api/courses/urls.py
+++ nolsatu_courses/api/courses/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('detail/<int:id>', views.CourseDetailView.as_view(), name='detail'),
     path('detail/<int:id>/enroll/', views.CourseEnrollDetailView.as_view(), name='detail_enroll'),
     path('<int:id>/enroll/', views.EnrollCourseView.as_view(), name='enroll'),
-    path('preview-list/<int:id>', views.CoursePreviewListView.as_view(), name='preview_list'),
-    path('module-preview/<int:id>', views.ModulePreviewView.as_view(), name='module_preview'),
-    path('section-preview/<int:id>', views.SectionPreviewView.as_view(), name='section_preview'),
+    path('preview/list/<int:id>', views.CoursePreviewListView.as_view(), name='preview_list'),
+    path('module/preview/<int:id>', views.ModulePreviewView.as_view(), name='module_preview'),
+    path('section/preview/<int:id>', views.SectionPreviewView.as_view(), name='section_preview'),
 ]
