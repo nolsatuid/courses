154b389530b9aa8b1dfea61a06c87a8f1da6e13e
irfanpule <irfan.pule2@gmail.com>
2020-04-02 16:19:33 +0700

    apperance from academy

--- nolsatu_courses/apps/context_processors.py
+++ nolsatu_courses/apps/context_processors.py
@@ -1,13 +1,16 @@
 from django.conf import settings
+from django.core.cache import cache
 
 
 def nolsatu_context(request):
-
+    apperance = cache.get(settings.KEY_CACHE_APPERANCE, {})
     return {
         'nolsatu_profile_page_url': settings.NOLSATU_PROFILE_PAGE_URL,
         'nolsatu_home_page_url': settings.NOLSATU_HOST,
-        'image_logo': settings.IMG_LOGO,
-        'brand': settings.BRAND,
-        'color_theme': settings.COLOR_THEME,
-        'sidebar_color': settings.SIDEBAR_COLOR,
+        'brand_logo': apperance.get('logo', ''),
+        'brand_logo_light': apperance.get('logo_light', ''),
+        'brand_logo_dark': apperance.get('logo_dark', ''),
+        'brand': apperance.get('site_name', 'NolSatu'),
+        'color_theme': apperance.get('color_theme', 'danger'),
+        'sidebar_color': apperance.get('sidebar_color', 'light'),
     }
