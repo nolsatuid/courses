2ca1534a2569b3de308a1d61130eb0d3c2fe4696
alinnural <alin@btech.co.id>
2020-04-24 21:04:13 +0700

    cru quiz

--- nolsatu_courses/backoffice/quizzes/views.py
+++ nolsatu_courses/backoffice/quizzes/views.py
@@ -1,9 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
 
 from quiz.models import Quiz, Sitting
+from quiz.admin import QuizAdminForm
+
+
+@staff_member_required
+def index(request):
+    context = {
+        'menu_active': 'quiz',
+        'title': _('Daftar Kuis'),
+        'quizzes': Quiz.objects.all(),
+        'sidebar': True
+    }
+    return render(request, 'backoffice/quizzes/index.html', context)
+
+
+@staff_member_required
+def add(request):
+    form = QuizAdminForm(data=request.POST or None)
+    if form.is_valid():
+        quiz = form.save()
+        messages.success(request, _(f"Berhasil tambah kuis {quiz.title}"))
+        return redirect('backoffice:quizzes:index')
+
+    context = {
+        'menu_active': 'quiz',
+        'title': _('Tambah Kuis'),
+        'form': form,
+        'title_submit': 'Simpan'
+    }
+    return render(request, 'backoffice/form.html', context)
+
+
+@staff_member_required
+def edit(request, id):
+    quiz = get_object_or_404(Quiz, id=id)
+    form = QuizAdminForm(data=request.POST or None, instance=quiz)
+    if form.is_valid():
+        quiz = form.save()
+        messages.success(request, _(f"Berhasil ubah kursus {quiz.title}"))
+        return redirect('backoffice:quizzes:index')
+
+    context = {
+        'menu_active': 'quiz',
+        'title': _('Ubah Kuis'),
+        'form': form,
+        'title_submit': 'Simpan'
+    }
+    return render(request, 'backoffice/form.html', context)
 
 
 @staff_member_required
