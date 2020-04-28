ce9048dcd543225ad01dbee681be0670d75d7bbc
irfanpule <irfan.pule2@gmail.com>
2020-04-28 10:55:57 +0700

    validation start end time quiz

--- nolsatu_courses/backoffice/quizzes/forms.py
+++ nolsatu_courses/backoffice/quizzes/forms.py
@@ -1,9 +1,30 @@
+from django import forms
 from quiz.admin import QuizAdminForm
 
 
 class FormQuiz(QuizAdminForm):
 
     class Media:
-       css = {'all':('admin/css/widgets.css',),}
-       js = ('admin/jquery.js','/admin/jsi18n/')
-       
+        css = {'all': ('admin/css/widgets.css',)}
+        js = ('admin/jquery.js', '/admin/jsi18n/')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data['start_time']
+        end_time = cleaned_data['end_time']
+
+        if start_time and not end_time:
+            raise forms.ValidationError("Waktu selesai harus diisi")
+        elif not start_time and end_time:
+            raise forms.ValidationError("Waktu awal harus diisi")
+        elif start_time and end_time:
+            if end_time.date() > start_time.date():
+                raise forms.ValidationError(
+                    "Tanggal waktu selesai tidak boleh lebih besar"
+                    " dari tanggal waktu mulai")
+            elif end_time.time() <= start_time.time():
+                raise forms.ValidationError(
+                    "Jam waktu selesai tidak boleh lebih kecil atau"
+                    " sama dengan jam waktu mulai")
+
+        return cleaned_data
