c0f391916b18eaedf6c345634a8bf1e714b505b3
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-05-11 19:39:06 +0700

    fix file name max length

--- nolsatu_courses/apps/upload_files/models.py
+++ nolsatu_courses/apps/upload_files/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class UploadFile(models.Model):
     name = models.CharField(max_length=200)
-    file = models.FileField(upload_to="uploads/")
+    file = models.FileField(upload_to="uploads/", max_length=200)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
