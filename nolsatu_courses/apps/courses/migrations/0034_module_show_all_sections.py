080a51860cc1f983ada6729716194f9faa1df60b
irfanpule <irfan.pule2@gmail.com>
2020-05-13 14:20:29 +0700

    fix migration files

--- nolsatu_courses/apps/courses/migrations/0034_module_show_all_sections.py
+++ nolsatu_courses/apps/courses/migrations/0034_module_show_all_sections.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0032_auto_20200428_1354'),
+        ('courses', '0033_auto_20200508_0510'),
     ]
 
     operations = [
