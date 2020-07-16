c00796271fd1d9a85fdb8135e77c74ed16c792b8
Gutei <anggeraryp@gmail.com>
2020-07-16 11:30:16 +0700

    add model MemberNolSatu role field

--- nolsatu_courses/apps/accounts/models.py
+++ nolsatu_courses/apps/accounts/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from model_utils import Choices
 
 
 class MemberNolsatu(models.Model):
+    ROLE = Choices(
+        (1, 'student', 'Student'),
+        (2, 'trainer', 'Trainer'),
+        (3, 'company', 'Company'),
+        (4, 'vendor', 'Vendor'),
+    )
+
     user = models.OneToOneField(User, related_name='nolsatu', on_delete=models.CASCADE)
     id_nolsatu = models.IntegerField()
+    role = models.PositiveIntegerField(choices=ROLE, blank=True, null=True)
     avatar = models.CharField(max_length=250, null=True, blank=True)
     phone_number = models.CharField(max_length=50, null=True, blank=True)
 
