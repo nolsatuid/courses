dc7f00e5e0e149b858397ea6f34d699fc812cef0
Gutei <anggeraryp@gmail.com>
2020-07-17 23:18:14 +0700

    add features course-detail

--- nolsatu_courses/vendors/courses/urls.py
+++ nolsatu_courses/vendors/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'courses'
 urlpatterns = [
     path('', views.get_list_courses, name='index'),
+    path('details/<int:courses_id>', views.get_details_courses, name='details'),
 ]
--- nolsatu_courses/vendors/courses/views/courses.py
+++ nolsatu_courses/vendors/courses/views/courses.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404,render, redirect
 from nolsatu_courses.apps.courses.models import Courses
 
 
@@ -13,3 +13,14 @@ def get_list_courses(request):
         'sidebar': True
     }
     return render(request, 'vendors/courses/courses-list.html', context)
+
+
+@staff_member_required
+def get_details_courses(request, courses_id):
+    course = get_object_or_404(Courses, id=courses_id, vendor__users__email=request.user.email)
+    context = {
+        'menu_active': 'course',
+        'title': 'Detail Kursus',
+        'course': course,
+    }
+    return render(request, 'vendors/courses/course-detail.html', context)
