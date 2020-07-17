e24173c59c806738e75637e3d09a3c5135035c94
Gutei <anggeraryp@gmail.com>
2020-07-17 15:59:30 +0700

    changed view-index at vendor

--- nolsatu_courses/vendors/views.py
+++ nolsatu_courses/vendors/views.py
@@ -5,6 +5,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 @staff_member_required
 def index(request):
-    template_name = 'backoffice/index.html'
-    context = {}
+    template_name = 'vendors/index.html'
+    context = {
+        'menu_active': 'dashboard',
+    }
     return render(request, template_name, context)
