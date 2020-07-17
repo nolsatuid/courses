3742be089cc2f98f1f0a498f8e39b89f262d2a15
Gutei <anggeraryp@gmail.com>
2020-07-17 15:20:36 +0700

    add skeleton vendor

---
+++ nolsatu_courses/vendors/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin.views.decorators import staff_member_required
+from .forms import FormFilter
+
+
+@staff_member_required
+def index(request):
+    template_name = 'backoffice/index.html'
+    context = {}
+    return render(request, template_name, context)
