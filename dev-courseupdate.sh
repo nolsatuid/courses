21aa86c4be5a1593c1e4de1c7d51de74558db45d
Ananda <ananda@btech.co.id>
2020-07-19 23:30:03 +0700

    Update dev-courseupdate.sh

--- dev-courseupdate.sh
+++ dev-courseupdate.sh
@@ -3,6 +3,7 @@
 service_name="nolsatu_courses"
 
 cd /var/www/html/nolsatu-courses/
+source env/bin/activate
 git pull origin master &&
 pip install -r requirements.txt
 ./manage.py collecstatic --noinput
