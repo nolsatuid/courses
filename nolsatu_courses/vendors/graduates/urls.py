e7c446e13c80978e358ab49882a488c5767292e3
Gutei <anggeraryp@gmail.com>
2020-08-08 04:03:22 +0700

    add feature candidate graduates

---
+++ nolsatu_courses/vendors/graduates/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'graduates'
+urlpatterns = [
+    path('candidate/', views.candidate, name='candidate'),
+    path('candidate-to-graduate/<int:candidate_id>', views.candidate_to_graduate, name='candidate_to_graduate'),
+]
