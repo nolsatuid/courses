cb23d13acdb085375df23888da2526fa9671520e
Gutei <anggeraryp@gmail.com>
2020-09-14 17:11:33 +0700

    added featured add to cart

--- nolsatu_courses/website/carts/urls.py
+++ nolsatu_courses/website/carts/urls.py
@@ -5,4 +5,5 @@ app_name = 'carts'
 urlpatterns = [
     path('', views.cart, name='index'),
     path('<str:cart_id>/delete', views.cart_delete, name='cart_delete'),
+    path('<str:product_id>/add', views.add_item, name='add_item'),
 ]
--- nolsatu_courses/website/carts/views.py
+++ nolsatu_courses/website/carts/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
-from nolsatu_courses.apps.products.models import Cart
+from nolsatu_courses.apps.products.models import Cart, Product
 
 
 @login_required
@@ -30,4 +30,19 @@ def cart_delete(request, cart_id):
         carts = Cart.objects.filter(user=request.user)
         data['total'] = carts.annotate(final_price=F('product__price') - F('product__discount')
                                        ).aggregate(price=Sum('final_price'))
+    return JsonResponse(data, status=200)
+
+
+@login_required
+@transaction.atomic
+def add_item(request, product_id):
+    pick_product = get_object_or_404(Product, id=product_id)
+    data = dict()
+    if request.method == 'POST':
+        try:
+            item_in_cart = Cart.objects.get(product=pick_product, user=request.user)
+            data['message'] = _('Gagal Menambahkan, Kursus Sudah Ada Di Keranjang!')
+        except item_in_cart.DoesNotExist:
+            Cart(product=pick_product, user=request.user).save()
+
     return JsonResponse(data, status=200)
