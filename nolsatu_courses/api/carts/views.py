481e00462046fa05784793e75c1a53f8a0e71e71
Gutei <anggeraryp@gmail.com>
2020-10-01 17:01:23 +0700

    resolve mr

--- nolsatu_courses/api/carts/views.py
+++ nolsatu_courses/api/carts/views.py
@@ -14,8 +14,6 @@ from nolsatu_courses.apps.products.models import Product, Order, Cart
 from .serializers import AddCartSerializer
 
 
-# Here add your api view
-
 class AddToCartView(UserAuthAPIView):
     @swagger_auto_schema(tags=['Carts'], operation_description="Add to My Carts",
                          responses={status.HTTP_200_OK: AddCartSerializer()},
@@ -23,26 +21,28 @@ class AddToCartView(UserAuthAPIView):
     def post(self, request):
         data = request.data
 
-        if not data.get('product_id'):
+        serializer = AddCartSerializer(data=data)
+
+        if serializer.is_valid():
+            status_check = [Order.STATUS.created, Order.STATUS.pending, Order.STATUS.success]
+
+            pick_product = get_object_or_404(Product, id=data.get('product_id'))
+            user_order_item = pick_product.orderitem_set.filter(order__user=self.request.user).first()
+
+            try:
+                if pick_product.course.has_enrolled(user=self.request.user):
+                    return ErrorResponse(error_message=_('Gagal Menambahkan, Anda Telah terdaftar '
+                                                         'di dalam kursus!'))
+                elif user_order_item and user_order_item.order.status in status_check:
+                    return ErrorResponse(error_message=_('Gagal Menambahkan, Anda Telah Melakukan '
+                                                         'Pembelian Pada Kursus ini!'))
+                else:
+                    Cart.objects.get(product=pick_product, user=self.request.user)
+                    return ErrorResponse(error_message=_('Gagal Menambahkan, Kursus Sudah Ada '
+                                                         'di Keranjang!'))
+            except Cart.DoesNotExist:
+                Cart(product=pick_product, user=self.request.user).save()
+        else:
             return ErrorResponse(error_message=_('Gagal Menambahkan Kursus ke Keranjang'))
-
-        status_check = [Order.STATUS.created, Order.STATUS.pending, Order.STATUS.success]
-
-        pick_product = get_object_or_404(Product, id=data.get('product_id'))
-        user_order_item = pick_product.orderitem_set.filter(order__user=self.request.user).first()
-
-        try:
-            if pick_product.course.has_enrolled(user=self.request.user):
-                return ErrorResponse(error_message=_('Gagal Menambahkan, Anda Telah terdaftar '
-                                                     'di dalam kursus!'))
-            elif user_order_item and user_order_item.order.status in status_check:
-                return ErrorResponse(error_message=_('Gagal Menambahkan, Anda Telah Melakukan '
-                                                     'Pembelian Pada Kursus ini!'))
-            else:
-                Cart.objects.get(product=pick_product, user=self.request.user)
-                return ErrorResponse(error_message=_('Gagal Menambahkan, Kursus Sudah Ada '
-                                                     'di Keranjang!'))
-        except Cart.DoesNotExist:
-            Cart(product=pick_product, user=self.request.user).save()
 
         return Response({'message': _('Berhasil Menambahkan Kursus ke Keranjang')})
