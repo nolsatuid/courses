0d16ac2d7b85cc12e4558642b4d033ee324e8f0c
Gutei <anggeraryp@gmail.com>
2020-10-09 11:02:01 +0700

    resolve mr

--- nolsatu_courses/api/carts/urls.py
+++ nolsatu_courses/api/carts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add-item', views.AddToCartView.as_view(), name='add_item'),
     path('delete-item', views.DeleteItemCartView.as_view(), name='delete_item'),
     path('get-items', views.CartListView.as_view(), name='get_items'),
-    path('/checkout', views.CheckoutView.as_view(), name='checkout'),
+    path('checkout', views.CheckoutView.as_view(), name='checkout'),
 ]
