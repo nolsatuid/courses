5668fe78d995256bdfa3301cd6e5764c26c0d3d2
Gutei <anggeraryp@gmail.com>
2020-10-12 12:06:29 +0700

    add feature api detail order

--- nolsatu_courses/api/orders/serializers.py
+++ nolsatu_courses/api/orders/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from nolsatu_courses.apps.products.models import Order
+from nolsatu_courses.apps.products.models import Order, OrderItem
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -14,3 +14,26 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'number', 'status', 'created_at']
         read_only_fields = ['id', 'number', 'status', 'created_at']
+
+
+class OrderAllFieldsSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    def get_status(self, obj) -> str:
+        return obj.get_status_display()
+
+    class Meta:
+        model = Order
+        fields = ('id', 'user', 'number', 'status', 'tax', 'discount',
+                  'grand_total', 'remote_transaction_id',
+                  'created_at', 'updated_at')
+        read_only_fields = ('id', 'user', 'number')
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'price', 'name']
+        read_only_fields = ['id', 'price', 'name']
--- nolsatu_courses/api/orders/views.py
+++ nolsatu_courses/api/orders/views.py
@@ -1,13 +1,14 @@
 from drf_yasg.utils import swagger_auto_schema
 from requests import Response
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 from rest_framework.response import Response
 
 from nolsatu_courses.api.authentications import UserAuthAPIView
 from nolsatu_courses.apps.products.models import Order
 
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer, OrderAllFieldsSerializer, OrderItemSerializer
 
 
 class OrderListView(UserAuthAPIView):
@@ -17,6 +18,23 @@ class OrderListView(UserAuthAPIView):
         orders = Order.objects.filter(user=self.request.user)
         serializer = OrderSerializer(orders, many=True)
         resp = {
-             "orders": serializer.data,
+            "orders": serializer.data,
         }
         return Response(resp)
+
+
+class DetailOrderView(UserAuthAPIView):
+    @swagger_auto_schema(tags=['Orders'], operation_description="My Orders Detail",
+                         responses={status.HTTP_200_OK: ""}, )
+    def get(self, request, id):
+        order = get_object_or_404(Order, user=self.request.user, id=id)
+        order_items = order.orders.all()
+
+        serializer_order = OrderAllFieldsSerializer(order)
+        serializer_order_items = OrderItemSerializer(order_items, many=True)
+
+        data = {"order": serializer_order.data,
+                "order_items": serializer_order_items.data
+                }
+
+        return Response(data)
