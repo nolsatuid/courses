0bceaae19b6c8e57cb4e4b09b927f38950d640fb
Gutei <anggeraryp@gmail.com>
2020-11-10 09:27:05 +0700

    resolve mr

--- nolsatu_courses/apps/vouchers/models.py
+++ nolsatu_courses/apps/vouchers/models.py
@@ -1,28 +1,24 @@
-import uuid
-
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
-from ckeditor_uploader.fields import RichTextUploadingField
+from markdownx.models import MarkdownxField
 
-
-# Create your models here.
+from model_utils import Choices
 
 
 class Voucher(models.Model):
-    PROMO_TYPE = [
-        (1, _("Persentase")),
-        (2, _("Nilai")),
-    ]
+    PROMO_TYPE = Choices(
+        (1, 'percentage', _("Persentase")),
+        (2, 'value', _("Nilai")),
+    )
 
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     code = models.CharField(_("Kode Voucher"), max_length=50)
     discount_type = models.SmallIntegerField(_("Voucher Promo Tipe"), choices=PROMO_TYPE, blank=True, null=True)
     discount_value = models.IntegerField(_("Diskon yang Diberikan"), blank=True, null=True)
     discount = models.IntegerField(_("Nominal Diskon"), default=0)
-    description = RichTextUploadingField(_("Deskripsi Voucher Promo"), default="")
+    description = MarkdownxField(_("Deskripsi Voucher Promo"), default="")
     qty = models.PositiveIntegerField(_("Jumlah"), default=0)
     product = models.ManyToManyField('products.Product')
     start_date = models.DateTimeField(_('Tanggal Voucher Berlaku'), auto_now_add=True)
@@ -42,12 +38,11 @@ class Voucher(models.Model):
 
 
 class UserVoucher(models.Model):
-    STATUS = [
-        (1, _("Unused")),
-        (2, _("Used")),
-    ]
+    STATUS = Choices(
+        (1, "Used", _("Used")),
+        (2, "Unused", _("Unused")),
+    )
 
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE)
     voucher = models.ForeignKey(Voucher, verbose_name=_("Voucher"), on_delete=models.CASCADE)
     status = models.SmallIntegerField(choices=STATUS, default=STATUS)
