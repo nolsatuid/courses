7e06e6c1bfda9f03aed6c2c1e2bee889fe678a34
Gutei <anggeraryp@gmail.com>
2020-11-24 10:34:03 +0700

    resolve MR admin list display admin voucher

--- nolsatu_courses/apps/vouchers/admin.py
+++ nolsatu_courses/apps/vouchers/admin.py
@@ -5,9 +5,12 @@ from .models import *
 
 @admin.register(Voucher)
 class VoucherAdmin(admin.ModelAdmin):
-    list_display = ('code',)
+    list_display = ('code', 'get_products', 'qty', 'start_date', 'end_date')
+
+    def get_products(self, obj):
+        return ", ".join([p.course.title for p in obj.product.all()])
 
 
 @admin.register(UserVoucher)
 class UserVoucherAdmin(admin.ModelAdmin):
-    list_display = ('voucher',)
+    list_display = ('voucher', 'user', 'status')
