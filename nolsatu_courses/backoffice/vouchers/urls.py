1cdaec642e0e86ca6b85959ab37a922bb89efde4
Gutei <anggeraryp@gmail.com>
2020-11-24 19:11:58 +0700

    added delelete voucher

--- nolsatu_courses/backoffice/vouchers/urls.py
+++ nolsatu_courses/backoffice/vouchers/urls.py
@@ -5,4 +5,5 @@ app_name = 'vouchers'
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:id>', views.detail, name='detail'),
+    path('delete/<int:id>', views.delete, name='delete'),
 ]
--- nolsatu_courses/backoffice/vouchers/views.py
+++ nolsatu_courses/backoffice/vouchers/views.py
@@ -1,5 +1,8 @@
+import sweetify
+
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import get_object_or_404
+from django.db import transaction
+from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 
@@ -29,3 +32,12 @@ def detail(request, id):
         'voucher': voucher,
     }
     return render(request, 'backoffice/vouchers/detail.html', context)
+
+
+@superuser_required
+@transaction.atomic
+def delete(request, id):
+    voucher = get_object_or_404(Voucher, id=id)
+    voucher.delete()
+    sweetify.success(request, 'Berhasil hapus voucher', icon='success', button='OK')
+    return redirect('backoffice:vouchers:index')
