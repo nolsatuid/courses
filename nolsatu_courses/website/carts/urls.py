a9d5b4bd4f44492a8b04a2e54b445602ffd6a4a0
irfanpule <irfan.pule2@gmail.com>
2020-12-03 11:06:46 +0700

    add / on checkou and payment

--- nolsatu_courses/website/carts/urls.py
+++ nolsatu_courses/website/carts/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'carts'
 urlpatterns = [
     path('', views.cart, name='index'),
-    path('<str:cart_id>/delete', views.cart_delete, name='cart_delete'),
-    path('<str:product_id>/add', views.add_item, name='add_item'),
-    path('choose-item', views.choose_item, name='choose_item'),
-    path('checkout', views.checkout, name='checkout'),
-    path('payment', views.payment, name='payment'),
+    path('<str:cart_id>/delete/', views.cart_delete, name='cart_delete'),
+    path('<str:product_id>/add/', views.add_item, name='add_item'),
+    path('choose-item/', views.choose_item, name='choose_item'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('payment/', views.payment, name='payment'),
 ]
