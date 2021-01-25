d1f89b55bdb6704b1289b249fa82a577d552e437
Muhamad Anjar <arvanria@gmail.com>
2021-01-25 16:17:16 +0700

    add course teacher page

--- nolsatu_courses/teachersroom/urls.py
+++ nolsatu_courses/teachersroom/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'teachersroom'
 urlpatterns = [
     path('', views.index, name='index'),
+    path("courses/", include('nolsatu_courses.teachersroom.courses.urls'))
 ]
